2d2a0898e797c8a5d4d40f405246c62ae36f8f01
ale0077 <103170531+ale0077@users.noreply.github.com>
2022-04-07 03:04:59 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = '5181652323:AAGhZtY_Yc52XYqMg8OtBf9Zf4ZPnnp9sRg'
+API_ID = 18693993
+API_HASH = '382ee6b53bdd0df66a52ea9779c62424'
+
+
+# DATOS DEL MOODLE
+USUARIO = 'darian.borges@estudiantes.fbio.uh.cu'
+PASSWORD = 'darian1995'
+USUARIO_ID = 32581
+ZIP_MB = 100
+MOODLE_URL = 'evea.uh.cu'
+OWNER = [1511485191]
+
+# CUENTA DE MEGA
+GMAIL_MEGA = 'yanco148@gmail.com'
+PASS_MEGA = 'yancarlos1482005'
